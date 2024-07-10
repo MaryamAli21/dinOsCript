4067bc35864929c8edbd21976258590bf0e63e8f
MaryamAli21e <maryamali2188@gmail.com>
2024-07-10 15:47:19 +0300

    MVP ready

--- dinosCript.js
+++ dinosCript.js
@@ -1,3 +1,16 @@
+const logo = document.querySelector('#logo')
+// console.log('logo here ' + logo)
+const logoSound = document.querySelector("#logoSound"); 
+console.log('sound here ' + logoSound)
+
+function playLogoAudio() { 
+  // console.log('salman')
+  logoSound.play(); 
+} 
+
+logo.addEventListener('click', playLogoAudio)
+
+
 var container = document.querySelector(".text");
 
 var speeds = {
@@ -9,11 +22,11 @@ var speeds = {
 };
 
 var textLines = [
-   { speed: speeds.slow, string: "Oh, hello!" },
+   { speed: speeds.slow, string: "Hi, I am dinosCript.!!" },
    { speed: speeds.pause, string: "", pause: true },
-   { speed: speeds.normal, string: "Have you seen my pet" },
-   { speed: speeds.fast, string: "frog", classes: ["green"] },
-   { speed: speeds.normal, string: "around?" }
+   { speed: speeds.normal, string: "Mind my  bombastic eye! am not sketchy! I was born like this!" },
+   { speed: speeds.fast, string: "Let's Test your Javascript knowledge!", classes: ["green"] },
+   { speed: speeds.normal, string: "You ready!" }
 ];
 
 
@@ -55,98 +68,4 @@ function revealOneCharacter(list) {
 setTimeout(() => {
    revealOneCharacter(characters);   
 }, 600)
-
-
-
-const questions = [
-  {
-    question: "1. What reflects an understanding about JavaScript?",
-    choices: ["JavaScript is a popular web scripting language and is used for client-side and server-side development", " it's another C-style language", "JavaScript is not truly object oriented because it does not provide inheritance"],
-    correct: 0
-  },
-  {
-    question: "2. What alligns a difference between JavaScript and Java?",
-    choices: ["Javascript is an object-oriented programming language while java is an object-oriented scripting language", "Javascript source code needs a compiler before it can be ready to run in realtime while Java does not need compilation before running the application code", "JavaScript applications are meant to run inside a web browser while Java applications are generally made for use in operating systems and virtual machines"],
-    correct: 2
-  },
-  {
-    question: "3. What are the various data types that exist in JavaScript?",
-    choices: ["there are no types any value entry can be converted into a type!", "Boolean - For true and false values, Null - For empty or unknown values, Undefined - For variables that are only declared and not defined or initialized, Number - For integer and floating-point numbers, String - For characters and alphanumeric values, Object - For collections or complex values, Symbols - For unique identifiers for objects", "date, string, number, boolean, object"],
-    correct: 1
-  },
-  {
-    question: "4. What are the features of JavaScript?",
-    choices: ["javaScript allow the reading or writing of files on client machines, JavaScript allow the writing of files on server machines", "Lightweight, interpreted programming language, Cross-platform compatible, Open-source, Object-oriented, Integration with other backend and frontend technologies, Used especially for the development of network-based applications", "JavaScript close a window that it hasn't opened, JavaScript read information from an opened Web page that came from another server"],
-    correct: 1
-  },
-  {
-    question: "5. What are advantages of JavaScript over other web technologies?",
-    choices: ["Enhanced Interaction, Quick Feedback, Rich User Interface, Frameworks", "javascript develops large applications", "in JavaScript the code is not visible to everyone and anyone to view"],    
-    correct: 0
-  },
-
-  {
-    question: "6. How do you create an object in JavaScript?",
-    choices: [
-      "const student = { name: 'John’, age: 17 };", "const count = 0; for (let i = 0; i < 10; i++) { count = count + i; } console.log(count);", "both of the above are correct"],
-    correct: 2,
-  },
-  {
-    question: "7. How do you create an array in JavaScript?",
-    choices: ["var a = [];  var b = [‘a’, ‘b’, ‘c’, ‘d’, ‘e’];", "var group = {}; var b = [‘a’, ‘b’, ‘c’, ‘d’, ‘e’];", "cvar a = [] , var b = ‘a’, ‘b’, ‘c’, ‘d’, ‘e’"],
-    correct: 0,
-  },
-  
-];
-
-// const logo = document.querySelector('#logo')
-// // console.log('logo here ' + logo)
-// const logoSound = document.querySelector("#logoSound"); 
-// console.log('sound here ' + logoSound)
-
-// function playLogoAudio() { 
-//   // console.log('salman')
-//   logoSound.play(); 
-// } 
-
-// logo.addEventListener('click', playLogoAudio)
-
-
-let currentQuestion = 0;
-let correctAnswers = 0;
-
-function showQuestion() {
-  const questionText = document.getElementById("question-text");
-  questionText.textContent = questions[currentQuestion].question;
-
-  const choices = document.querySelectorAll(".choice");
-  choices.forEach((choice, index) => {
-    choice.textContent = questions[currentQuestion].choices[index];
-  });
-
-  const feedback = document.getElementById("feedback");
-  feedback.textContent = "";
-}
-
-function checkAnswer(selected) {
-  const feedback = document.getElementById("feedback");
-  if (selected === questions[currentQuestion].correct) {
-    feedback.textContent = "Correct!";
-    correctAnswers++;
-  } else {
-    feedback.textContent = "Incorrect!";
-  }
-  setTimeout(() => {
-    currentQuestion++;
-
-    if (currentQuestion < questions.length) {
-      showQuestion();
-    } else {
-      const quizContainer = document.querySelector(".quiz-container");
-      quizContainer.innerHTML = `<p>You got ${correctAnswers} out of ${questions.length} questions.</p>`;
-    }
-  }, 2000);
-}
-
-showQuestion();
 
